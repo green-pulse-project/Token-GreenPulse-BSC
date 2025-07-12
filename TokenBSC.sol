93e76036dd533646811dbea363102bf77094e586
Green Pulse  | Token <greenpulse.c@gmail.com>
2025-07-12 10:04:41 -0500

    Update TokenBSC.sol

--- TokenBSC.sol
+++ TokenBSC.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract GreenPulse is ERC20 {
+    constructor() ERC20("Green Pulse", "GPLS") {
+        uint256 initialSupply = 100_000_000 * 10 ** decimals(); // 100 millones
+        _mint(msg.sender, initialSupply); // Minta los tokens directamente al desplegador
+    }
+}
 
